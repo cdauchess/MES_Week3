fd2ae87f9a2c552f4b744feb5993c784a932697a
cdauchess <dauchess@vt.edu>
2022-09-16 19:28:32 -0400

    Update main.c
    blinky LED with a button to enable/disable.  Not debouncing or interrupt yet.

--- main.c
+++ main.c
@@ -10,6 +10,13 @@
 #include "pico/binary_info.h"
 
 const uint LED_PIN = 25;
+const uint BUTTON_PIN = 15;
+typedef enum{
+    Blinking,
+    NoBlink
+}   LED_STATE;
+
+
 
 int main() {
     bi_decl(bi_program_description("PROJECT DESCRIPTION"));
@@ -19,11 +26,38 @@ int main() {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
 
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_down(BUTTON_PIN);
+
+    LED_STATE LED = NoBlink;
+
     while(1) {
-        gpio_put(LED_PIN, 0);
-        sleep_ms(250);
-        gpio_put(LED_PIN, 1);
-        puts("Hello World\n");
-        sleep_ms(1000);
+
+        switch(LED)
+        {
+            case Blinking:
+                gpio_put(LED_PIN, 0);
+                sleep_ms(250);
+                gpio_put(LED_PIN, 1);
+                puts("Hello World\n");
+                sleep_ms(500);
+                if(gpio_get(BUTTON_PIN) != 0)
+                {
+                    LED = NoBlink;
+                    gpio_put(LED_PIN,0);
+                    sleep_ms(100);
+                }
+                break;
+            case NoBlink:
+                
+                if(gpio_get(BUTTON_PIN) !=0)
+                {
+                    LED = Blinking;
+                    sleep_ms(100);
+                }
+                sleep_ms(50);
+                break;
+        }
     }
 }
